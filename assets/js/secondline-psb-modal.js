db507e70781fd2b602cdcb717bfa77649e689962
SecondLineThemes <support@SecondLineThemes.com>
2019-05-12 07:19:32 +0300

    updated to version 1.1.0

--- assets/js/secondline-psb-modal.js
+++ assets/js/secondline-psb-modal.js
@@ -1,15 +1,19 @@
 // OPEN/CLOSE MODAL
 
+
+// Removed for dynamic script per button to allow multiple buttons per page
+/*
 jQuery(document).ready(function($) {
 	 'use strict';
+
 	
-		
 	$('.podcast-subscribe-button').on("click", function() {
-		$("#secondline-psb-subs-modal").modal({
+		$("#secondline-psb-subs-modal.modal").modal({
 			fadeDuration: 250,
 			closeText: '',
-	  });
-	  return false;
-	});    
+		});
+		return false;
+	}); 
+	
 
-});
+});*/
